161c13dd349de77c8c476f976db9d17737362a54
SilviuDN <42895950+SilviuDN@users.noreply.github.com>
2021-01-10 09:57:18 +0100

    Refactoring. reset() function refactored.

--- app.js
+++ app.js
@@ -33,7 +33,6 @@ function updateScores(player, opponent){
 }
 
 p1.button.addEventListener('click', function(){
-    console.log("p1");
     updateScores(p1, p2); 
 })
 p2.button.addEventListener('click', function(){
@@ -49,12 +48,10 @@ resetButton.addEventListener('click', reset)
 
 function reset(){
     isGameOver = false;
-    p2.score = 0;
-    p2.display.textContent = 0;    
-    p1.score = 0;
-    p1.display.textContent = 0;  
-    p1.display.classList.remove('has-text-success','has-text-danger');
-    p2.display.classList.remove('has-text-success','has-text-danger');
-    p1.button.disabled = false;
-    p2.button.disabled = false;
+    for(let p of [p1,p2]){
+        p.score = 0;
+        p.display.textContent = 0;  
+        p.display.classList.remove('has-text-success','has-text-danger');
+        p.button.disabled = false;
+    }
 }
